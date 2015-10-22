1dd222c8756924c0288efb6cdd461f6a5ed64f23
73rhodes <drderidder@gmail.com>
2015-10-21 22:27:26 -0400

    use map and lint

--- js/messenger.js
+++ js/messenger.js
@@ -1,50 +1,50 @@
 // messenger.js
 
-window.messenger = window.top.messenger || new Messenger();
+/*global window*/
 
-window.onbeforeunload = function () {
-  window.messenger.unsubscribe(window);
-}
+"use strict";
 
 function Messenger() {
-  this.state = {};
-  this.subscribers = [];
+    this.state = {};
+    this.subscribers = [];
 }
 
 Messenger.prototype.publish = function (channel, message) {
-  if (message.state) {
-    this.state[channel] = message.state;
-    this.notify(channel, this.state[channel]);
-  } else if (message.eventData) {
-    this.notify(channel, message.eventData);
-  } 
+    if (message.state) {
+        this.state[channel] = message.state;
+        this.notify(channel, this.state[channel]);
+    } else if (message.eventData) {
+        this.notify(channel, message.eventData);
+    }
 };
 
 Messenger.prototype.notify = function (channel, data) {
-  this.subscribers.forEach(function(subscriber) {
-    if (subscriber.channel == channel) {
-      subscriber.callback(data);
-    }
-  });
+    this.subscribers.forEach(function (subscriber) {
+        if (subscriber.channel === channel) {
+            subscriber.callback(data);
+        }
+    });
 };
 
-Messenger.prototype.subscribe = function (channel, cbwindow, cb) {
-  this.subscribers.push({
-    "channel": channel,
-    "callback": cb,
-    "window": cbwindow
-  });
-  if (this.state[channel]) {
-    cb(this.state[channel]);
-  }
+Messenger.prototype.subscribe = function (channel, win, cb) {
+    this.subscribers.push({
+        "channel": channel,
+        "callback": cb,
+        "window": win
+    });
+    if (this.state[channel]) {
+        cb(this.state[channel]);
+    }
 };
 
-Messenger.prototype.unsubscribe = function (cbwindow) {
-  var updatedSubscribers = [];
-  this.subscribers.forEach(function (subscriber) {
-    if (subscriber.window !== cbwindow) {
-      updatedSubscribers.push(subscriber);
-    }
-  });
-  this.subscribers = updatedSubscribers;
+Messenger.prototype.unsubscribe = function (win) {
+    this.subscribers = this.subscribers.filter(function (subscriber) {
+        return subscriber.window !== win;
+    });
+};
+
+window.messenger = window.top.messenger || new Messenger();
+
+window.onbeforeunload = function () {
+    window.messenger.unsubscribe(window);
 };
